39e962fa7ec92e10c63eac0986c62168fdd85d30
Phannawich Jadpotwanich <phannawich1122@gmail.com>
2026-01-01 20:59:55 -0600

    basicDataType file

---
+++ basicDataType.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+    printf("%i\n", i);
+    printf("%ld\n", l);
+    printf("%c\n", c);  
+    printf("%f\n", f);
+    printf("%lf\n", d);
+    return 0;
+}
