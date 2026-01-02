13f963db9377d329fa267020f2e7488e7bfccf3f
Phannawich Jadpotwanich <phannawich1122@gmail.com>
2026-01-01 22:02:27 -0600

    add .cpp at the end of loop and pointer, add array

---
+++ arrayIntro.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+    }
+    for (int i = (n - 1); i >= 0; i--){
+        std::cout << arr[i] << " ";
+    }
+    return 0;
+}
+    
