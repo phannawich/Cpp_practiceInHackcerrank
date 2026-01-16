94bd2d5a400977fb643835aa659ccdf0d0918892
Phannawich Jadpotwanich <phannawich1122@gmail.com>
2026-01-15 16:47:57 -0800

    [Count Elements Greater Than Previous Average] redo in C++20

---
+++ SWE-prep/countElementsGreaterThanPreviousAverage.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+/*
+ * Complete the 'countResponseTimeRegressions' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY responseTimes as parameter.
+ */
+
+int countResponseTimeRegressions(vector<int> responseTimes) {
+    int output = 0;
+    
+    if(responseTimes.size() == 0 || responseTimes.size() == 1){
+        return output;
+    }
+    
+    long long sum = responseTimes[0];
+    int avg;
+    
+    
+    for (size_t i = 1; i < responseTimes.size() ; i++) {
+        sum += responseTimes[i];
+        avg = sum / (i+1);
+        if(responseTimes[i] > avg){
+            output++;
+        }
+    }
+    
+    return output;     
+}
+
+int main()
+{
+    string responseTimes_count_temp;
+    getline(cin, responseTimes_count_temp);
+
+    int responseTimes_count = stoi(ltrim(rtrim(responseTimes_count_temp)));
+
+    vector<int> responseTimes(responseTimes_count);
+
+    for (int i = 0; i < responseTimes_count; i++) {
+        string responseTimes_item_temp;
+        getline(cin, responseTimes_item_temp);
+
+        int responseTimes_item = stoi(ltrim(rtrim(responseTimes_item_temp)));
+
+        responseTimes[i] = responseTimes_item;
+    }
+
+    int result = countResponseTimeRegressions(responseTimes);
+
+    cout << result << "\n";
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
